65f9a3e3c7bf8cb690cf5b57ea308308c212dc1b
水兵 <shuibing@shuibingdeMacBook-Pro.local>
2023-03-30 20:56:38 +0800

    [dump_nodes_features] dump func_name, block_name

--- svf-llvm/tools/IVFG_augmentation/augmentation.cpp
+++ svf-llvm/tools/IVFG_augmentation/augmentation.cpp
@@ -8,16 +8,78 @@
 using namespace std;
 using namespace SVF;
 
+static inline void ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+                return !std::isspace(ch);
+            }));
+}
+
+// trim from end (in place)
+static inline void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+                return !std::isspace(ch);
+            }).base(), s.end());
+}
+
+// trim from both ends (in place)
+static inline void trim(std::string &s) {
+    rtrim(s);
+    ltrim(s);
+}
+
+void replaceAll(std::string& str, const std::string& from, const std::string& to) {
+    if(from.empty())
+        return;
+    size_t start_pos = 0;
+    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
+    }
+}
+
+std::map<std::string, std::string> get_loc_dict(SVFGNode* node){
+    std::map<std::string, std::string> dict;
+    if (node->getICFGNode()->getFun() == nullptr){
+        dict["func_name"] = "GLOBAL";
+    }else{
+        dict["func_name"] = node->getICFGNode()->getFun()->getName();
+    }
+    if (node->getICFGNode()->getBB() == nullptr){
+        dict["block_name"] = "GLOBAL";
+    }else
+    {
+        dict["block_name"] = node->getICFGNode()->getBB()->getName();
+    }
+    return dict;
+}
+
+static std::string dict2str(std::map<std::string, std::string> dict){
+    std::string str;
+    std::stringstream  rawstr(str);
+    for(auto &it: dict){
+        trim(it.second);
+    }
+    //print
+    rawstr << "node_feature: {";
+    for(auto& item: dict){
+        rawstr << "\'" << item.first << "\'" << " : " << "\'" << item.second << "\', ";
+    }
+    rawstr << "}";
+    return rawstr.str();
+}
+
+#include<stdio.h>
 void dump_nodes_features(SVFG* svfg){
-    for(int i = 0; i < svfg->getSVFGNodeNum(); i++){
+    for(SVF::u32_t i = 0; i < svfg->getSVFGNodeNum(); i++){
         SVFGNode * node = svfg->getSVFGNode(i);
+        auto dict = get_loc_dict(node);
+        printf("%s\n", dict2str(dict).c_str());
     }
 }
 
 
 int main(int argc, char ** argv)
 {
-
     char **arg_value = new char*[argc];
     std::vector<std::string> moduleNameVec;
     moduleNameVec = OptionBase::parseOptions(
@@ -42,12 +104,9 @@ int main(int argc, char ** argv)
 
     SVFGBuilder svfBuilder(true);
     SVFG* svfg = svfBuilder.buildFullSVFG(ander);
-    //svfg->dump("/home/shuibing/ptCode2vec/SVFRemote/cmake-build-debug/bin/haha");
-    /// Collect uses of an LLVM Value
-    for(SVF::u32_t i = 0; i < svfg->getSVFGNodeNum(); i++){
-        std::cout << svfg->getSVFGNode(i)->toString() << "\n\n";
-    }
 
+    ///dump node features
+    dump_nodes_features(svfg);
 
     AndersenWaveDiff::releaseAndersenWaveDiff();
     SVFIR::releaseSVFIR();
